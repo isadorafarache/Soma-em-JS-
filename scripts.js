e704db6cfb3750c315f295b5623f6a05cb81b2be
Isadora <farachedev@gmail.com>
2022-06-22 21:59:31 -0300

    Soma-em-JS

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var n1 = prompt("Digite um número");
+n1 = parseFloat(n1);
+var n2 = prompt("Digite outro número");
+n2 = parseFloat(n2);
+var resultado = parseFloat(n1)+parseFloat(n2);
+alert ("A soma dos números é " + resultado);
+
+//Problema: pedir para o usuário digitar dois números e mostrar a soma//
+//Solução: Criei 3 var, duas para armazenar os números e uma para poder exibir o resultado//
+//Mas antes tive que converter as var n1 e n2 em "number" porque elas eram "string"//
+
