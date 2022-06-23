b85750e5aecdf6cb94f592e5349f85e5377f3257
Isadora Farache <FaracheDev@gmail.com>
2022-06-22 22:53:29 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,9 @@ n2 = parseFloat(n2);
 var resultado = parseFloat(n1)+parseFloat(n2);
 alert ("A soma dos números é " + resultado);
 
-//Problema: pedir para o usuário digitar dois números e mostrar a soma//
-//Solução: Criei 3 var, duas para armazenar os números e uma para poder exibir o resultado//
-//Mas antes tive que converter as var n1 e n2 em "number" porque elas eram "string"//
+/*
+Problema: pedir para o usuário digitar dois números e mostrar a soma.
+Solução: Criei 3 var, duas para armazenar os números e uma para poder exibir o resultado.
+Mas antes tive que converter as var n1 e n2 em "number" porque elas eram "string".
+*/
 
